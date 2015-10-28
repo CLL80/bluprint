c6fdb3438f9719a23b64e088ddd0c7822997b401
Tyler Martinez <slightlytyler@gmail.com>
2015-10-28 00:05:03 -0500

    more realistic dummy blueprint

--- dummy/blueprints/component.js
+++ dummy/blueprints/component.js
@@ -1,3 +1,24 @@
-export default {
-  key: 'it worked!!!';
+'use strict'
+
+import {
+  Component,
+  PropTypes,
+  View,
+} from 'react-native'
+
+import shouldPureComponentUpdate from 'react-pure-render/function'
+
+export default class AddNewButton extends Component {
+  shouldComponentUpdate = shouldPureComponentUpdate;
+
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+AddNewButton.propTypes = {
+
 };
