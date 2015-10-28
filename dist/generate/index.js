75f70dbb8f7115189fdef6bc7dd480cb0fc70311
Tyler Martinez <slightlytyler@gmail.com>
2015-10-27 23:45:58 -0500

    copying files almost working

--- dist/generate/index.js
+++ dist/generate/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
-    value: true
+  value: true
 });
 exports['default'] = generate;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
 
 var _path = require('path');
 
@@ -15,9 +19,9 @@ var _mkdirp = require('mkdirp');
 
 var _mkdirp2 = _interopRequireDefault(_mkdirp);
 
-var _fs = require('fs');
+var _findit = require('findit');
 
-var _fs2 = _interopRequireDefault(_fs);
+var _findit2 = _interopRequireDefault(_findit);
 
 var _chalk = require('chalk');
 
@@ -34,30 +38,55 @@ var _colors2 = _interopRequireDefault(_colors);
 var log = (0, _chip2['default'])();
 
 function generate(args) {
-    log('installing');
+  log('installing');
 
-    var blueprint = args[0];
+  var blueprintsRoot = 'dummy/blueprints';
+  var blueprintName = args[0];
 
-    var path = args[1];
-    var destinationRoot = 'dummy/app';
-    var directory = destinationRoot + '/' + _path2['default'].dirname(path);
-    var fileName = destinationRoot + '/' + path + '.js';
+  var path = args[1];
+  var destinationRoot = 'dummy/app';
+  var destinationDirectory = destinationRoot + '/' + _path2['default'].dirname(path);
+  var destinationPath = destinationRoot + '/' + path;
 
-    createDirectory(directory, createFile(fileName));
+  getBlueprintPath(blueprintsRoot, blueprintName, function (blueprintPath) {
+    return createDirectory(destinationDirectory, copyFile(blueprintPath, destinationPath));
+  });
 }
 
-var createDirectory = function createDirectory(directory, callback) {
-    return (0, _mkdirp2['default'])(directory, {}, callback);
+var getBlueprintPath = function getBlueprintPath(root, name, callback) {
+  var blueprintFinder = (0, _findit2['default'])(root);
+
+  blueprintFinder.on('file', function (file) {
+    var fileName = _path2['default'].parse(file).name;
+
+    if (fileName === name) {
+      callback(file);
+    }
+  });
 };
 
-var createFile = function createFile(fileName, callback) {
-    return _fs2['default'].exists(fileName, function (exists) {
-        if (exists) {
-            console.log('already exists');
-        } else {
-            _fs2['default'].writeFile(fileName, 'YESSSS');
-            log.info(_chalk2['default'].green('  create ') + fileName);
-        }
-    });
+var createDirectory = function createDirectory(directory, callback) {
+  return (0, _mkdirp2['default'])(directory, {}, callback);
+};
+
+var copyFile = function copyFile(source, targetPath, cb) {
+  var target = targetPath + _path2['default'].extname(source);
+
+  var rd = _fs2['default'].createReadStream(source);
+  rd.on("error", function (err) {
+    return done(err);
+  });
+
+  var wr = _fs2['default'].createWriteStream(target);
+  wr.on("error", function (err) {
+    return done(err);
+  });
+  wr.on("close", function () {
+    return done();
+  });
+
+  rd.pipe(wr);
+
+  function done(err) {}
 };
 module.exports = exports['default'];
--- src/generate/index.js
+++ src/generate/index.js
@@ -1,8 +1,9 @@
 'use strict'
 
+import fs from 'fs';
 import pathUtil from 'path';
 import mkdirp from 'mkdirp';
-import fs from 'fs';
+import find from 'findit'
 
 import chalk from 'chalk';
 import chip from 'chip';
@@ -13,24 +14,48 @@ const log = chip();
 export default function generate(args) {
   log('installing');
 
-  const blueprint = args[0];
+  const blueprintsRoot = 'dummy/blueprints';
+  const blueprintName = args[0];
 
   const path = args[1];
   const destinationRoot = 'dummy/app';
-  const directory = `${destinationRoot}/${pathUtil.dirname(path)}`;
-  const fileName =  `${destinationRoot}/${path}.js`;
+  const destinationDirectory = `${destinationRoot}/${pathUtil.dirname(path)}`;
+  const destinationPath =  `${destinationRoot}/${path}`;
 
-  createDirectory(directory, createFile(fileName));
+  getBlueprintPath(blueprintsRoot, blueprintName, (blueprintPath) =>
+    createDirectory(destinationDirectory,
+      copyFile(blueprintPath, destinationPath)
+    )
+  );
+}
+
+const getBlueprintPath = (root, name, callback) => {
+  const blueprintFinder = find(root);
+
+  blueprintFinder.on('file', file => {
+    let fileName = pathUtil.parse(file).name;
+
+    if (fileName === name) {
+      callback(file);
+    }
+  });
 }
 
 const createDirectory = (directory, callback) => mkdirp(directory, {}, callback);
 
-const createFile = (fileName, callback) =>
-    fs.exists(fileName, function (exists) {
-      if(exists){
-          console.log('already exists');
-      } else {
-          fs.writeFile(fileName, 'YESSSS');
-          log.info(chalk.green('  create ') + fileName);
-      }
-    });
+const copyFile = (source, targetPath, cb) => {
+  const target = targetPath + pathUtil.extname(source);
+
+  const rd = fs.createReadStream(source);
+  rd.on("error", err => done(err));
+
+  const wr = fs.createWriteStream(target);
+  wr.on("error", err => done(err));
+  wr.on("close", () => done());
+
+  rd.pipe(wr);
+
+  function done(err) {
+
+  }
+}
