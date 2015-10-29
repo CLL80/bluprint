c92b526246898f575012db7e9c3dbec2d272f241
Tyler Martinez <slightlytyler@gmail.com>
2015-10-29 18:53:54 -0500

    Fix dummy blueprints

--- dummy/blueprints/container/container.js
+++ dummy/blueprints/container/container.js
@@ -5,7 +5,7 @@ import { bindActionCreators, compose } from 'redux'
 
 import shouldPureComponentUpdate from 'react-pure-render/function'
 
-import <% PATH_TITLE_CASE %>Component from './component'
+import <% TEMPLATE_TOKEN titleCase %>Component from './component'
 
 function mapStateToProps(state) {
   return {
@@ -27,4 +27,4 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
   mergeProps
-)(<% PATH_TITLE_CASE %>Component);
+)(<% TEMPLATE_TOKEN titleCase %>Component);
