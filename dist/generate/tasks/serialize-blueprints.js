bff2ad3fabbb82dcb27c30be86708876f0901375
Tyler Martinez <slightlytyler@gmail.com>
2015-11-01 20:02:00 -0600

    Cleanup

---
+++ dist/generate/tasks/serialize-blueprints.js
@@ -0,0 +1,63 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = serializeBlueprints;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _findit = require('findit');
+
+var _findit2 = _interopRequireDefault(_findit);
+
+var _lodash = require('lodash');
+
+function serializeBlueprints(__blueprintRoot__, __blueprintType__, callback) {
+  var __blueprintDir__ = _path2['default'].join(__blueprintRoot__, __blueprintType__);
+  var directoryFinder = (0, _findit2['default'])(__blueprintDir__);
+  var blueprints = {
+    root: {
+      files: []
+    }
+  };
+
+  directoryFinder.on('directory', function (dir) {
+    if (dir !== __blueprintDir__) {
+      var relativeDir = dir.replace(__blueprintDir__, '').replace(/^\//, '');
+      var keyHierarchy = relativeDir.replace(_path2['default'].sep, '.');
+
+      (0, _lodash.set)(blueprints.root, keyHierarchy, { files: [] });
+    }
+  });
+
+  directoryFinder.on('end', function () {
+    var fileFinder = (0, _findit2['default'])(__blueprintDir__);
+
+    fileFinder.on('file', function (file) {
+      var fileName = _path2['default'].parse(file).name;
+
+      if (fileName !== 'config') {
+        var dir = _path2['default'].dirname(file);
+        var relativeDir = dir.replace(__blueprintDir__, '').replace(/^\//, '');
+        var keyHierarchy = dir === __blueprintDir__ ? 'root.files' : 'root.' + relativeDir.replace(_path2['default'].sep, '.') + '.files';
+
+        var currentFiles = (0, _lodash.get)(blueprints, keyHierarchy);
+        (0, _lodash.set)(blueprints, keyHierarchy, [].concat(_toConsumableArray(currentFiles), [file]));
+      }
+    });
+
+    fileFinder.on('end', function () {
+      callback(blueprints);
+    });
+  });
+}
+
+;
+module.exports = exports['default'];
---
+++ src/generate/tasks/serialize-blueprints.js
@@ -0,0 +1,47 @@
+import path from 'path';
+import find from 'findit'
+
+import { set, get } from 'lodash';
+
+export default function serializeBlueprints(__blueprintRoot__, __blueprintType__, callback) {
+  const __blueprintDir__ = path.join(__blueprintRoot__, __blueprintType__);
+  const directoryFinder = find(__blueprintDir__);
+  var blueprints = {
+    root: {
+      files: []
+    }
+  };
+
+  directoryFinder.on('directory', dir => {
+    if (dir !== __blueprintDir__) {
+      let relativeDir = dir.replace(__blueprintDir__, '').replace(/^\//, '');
+      let keyHierarchy = relativeDir.replace(path.sep, '.');
+
+      set(blueprints.root, keyHierarchy, { files: [] });
+    }
+  });
+
+  directoryFinder.on('end', () => {
+    const fileFinder = find(__blueprintDir__);
+
+    fileFinder.on('file', file => {
+      let fileName = path.parse(file).name;
+
+
+      if (fileName !== 'config') {
+        let dir = path.dirname(file);
+        let relativeDir = dir.replace(__blueprintDir__, '').replace(/^\//, '');
+        let keyHierarchy = dir === __blueprintDir__ ?
+            'root.files' :
+            `root.${relativeDir.replace(path.sep, '.')}.files`;
+
+        let currentFiles = get(blueprints, keyHierarchy);
+        set(blueprints, keyHierarchy, [...currentFiles, file]);
+      }
+    });
+
+    fileFinder.on('end', () => {
+      callback(blueprints)
+    });
+  })
+};
