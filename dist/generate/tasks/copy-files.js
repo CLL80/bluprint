e4a993b9f7b09b32f004375193a85055b27811cd
Tyler Martinez <slightlytyler@gmail.com>
2015-11-03 17:46:23 -0600

    Basics of write protection

--- dist/generate/tasks/copy-files.js
+++ dist/generate/tasks/copy-files.js
@@ -19,6 +19,10 @@ var _smartStream = require('smart-stream');
 
 var _smartStream2 = _interopRequireDefault(_smartStream);
 
+var _promptly = require('promptly');
+
+var _promptly2 = _interopRequireDefault(_promptly);
+
 var _replaceTemplateVariables = require('./replace-template-variables');
 
 var _replaceTemplateVariables2 = _interopRequireDefault(_replaceTemplateVariables);
@@ -27,41 +31,78 @@ var _success = require('./success');
 
 var _success2 = _interopRequireDefault(_success);
 
-function copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__) {
-  blueprints.forEach(function (blueprint) {
-    var fileName = __templateName__ ? __templateName__ + _path2['default'].extname(blueprint) : _path2['default'].basename(blueprint);
-    var target = _path2['default'].join(__destinationDirectory__, fileName);
+function copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__, callback) {
+  var index = arguments.length <= 5 || arguments[5] === undefined ? 0 : arguments[5];
 
-    var rd = _fs2['default'].createReadStream(blueprint, { encoding: 'utf8' });
-    rd.on("error", function (err) {
-      return error(err);
-    });
+  var blueprint = blueprints[index];
 
-    var wr = _fs2['default'].createWriteStream(target);
-    wr.on("error", function (err) {
-      return error(err);
-    });
-    wr.on("close", function () {
-      return done(target);
-    });
+  if (blueprint) {
+    (function () {
+      // If a blueprint existes for the current index
+      var fileName = __templateName__ ? __templateName__ + _path2['default'].extname(blueprint) : _path2['default'].basename(blueprint);
+      var target = _path2['default'].join(__destinationDirectory__, fileName);
 
-    var handleTemplateVariables = new _smartStream2['default'].SmartStream('ReplaceTemplateVariables');
-    var __templateToken__ = __templateName__ ? _path2['default'].parse(fileName).name : __templateDirectory__;
+      var handleTemplateVariables = new _smartStream2['default'].SmartStream('ReplaceTemplateVariables');
+      var __templateToken__ = __templateName__ ? _path2['default'].parse(fileName).name : __templateDirectory__;
 
-    handleTemplateVariables.setMiddleware(function (data, callback) {
-      return (0, _replaceTemplateVariables2['default'])(data, __templateToken__, callback);
-    });
+      handleTemplateVariables.setMiddleware(function (data, middlewareCallback) {
+        return (0, _replaceTemplateVariables2['default'])(data, __templateToken__, middlewareCallback);
+      });
 
-    rd.pipe(handleTemplateVariables).pipe(wr);
-  });
+      _fs2['default'].stat(target, function (err, stat) {
+        if (err == null) {
+          // If a file at target already exists
+          _promptly2['default'].confirm('Overwrite ' + target + '?', function (err, confirmed) {
+            if (confirmed) {
+              // If user confirms overwrite
+              writeFromBlueprint(blueprint, target, handleTemplateVariables, function () {
+                return copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__, callback, index + 1);
+              });
+            } else {
+              // If user denies overwrite
+              console.log('Skipping ' + target);
 
-  var error = function error(err) {
-    return log.error(err);
-  };
-  var done = function done(target) {
-    return (0, _success2['default'])(target);
-  };
+              copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__, callback, index + 1);
+            }
+          });
+        } else {
+          // If no file at target exists
+          writeFromBlueprint(blueprint, target, handleTemplateVariables, function () {
+            return copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__, callback, index + 1);
+          });
+        }
+      });
+    })();
+  } else {
+    // If no blueprint exists for the current index
+    callback();
+  }
 }
 
 ;
+
+function writeFromBlueprint(blueprint, target, handleTemplateVariables, callback) {
+  var rd = _fs2['default'].createReadStream(blueprint, { encoding: 'utf8' });
+  rd.on("error", function (err) {
+    return error(err);
+  });
+
+  var wr = _fs2['default'].createWriteStream(target);
+  wr.on("error", function (err) {
+    return error(err);
+  });
+  wr.on("close", function () {
+    done(target);
+    callback();
+  });
+
+  rd.pipe(handleTemplateVariables).pipe(wr);
+}
+
+var error = function error(err) {
+  return log.error(err);
+};
+var done = function done(target) {
+  return (0, _success2['default'])(target);
+};
 module.exports = exports['default'];
--- src/generate/tasks/copy-files.js
+++ src/generate/tasks/copy-files.js
@@ -1,35 +1,92 @@
 import fs from 'fs';
 import path from 'path';
 import ss from 'smart-stream';
+import promptly from 'promptly';
 
 import replaceTemplateVariables from './replace-template-variables';
 import success from './success';
 
-export default function copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__) {
-  blueprints.forEach(blueprint => {
+export default function copyFiles(blueprints, __destinationDirectory__, __templateDirectory__, __templateName__, callback, index = 0) {
+  const blueprint = blueprints[index];
+
+  if (blueprint) {
+    // If a blueprint existes for the current index
     const fileName = __templateName__ ?
         __templateName__ + path.extname(blueprint) :
         path.basename(blueprint);
     const target = path.join(__destinationDirectory__, fileName);
 
-    const rd = fs.createReadStream(blueprint, { encoding: 'utf8' });
-    rd.on("error", err => error(err));
-
-    const wr = fs.createWriteStream(target);
-    wr.on("error", err => error(err));
-    wr.on("close", () => done(target));
-
     const handleTemplateVariables = new ss.SmartStream('ReplaceTemplateVariables');
     const __templateToken__ = __templateName__ ? path.parse(fileName).name : __templateDirectory__;
 
-    handleTemplateVariables.setMiddleware((data, callback) =>
-      replaceTemplateVariables(data, __templateToken__, callback)
+    handleTemplateVariables.setMiddleware((data, middlewareCallback) =>
+      replaceTemplateVariables(data, __templateToken__, middlewareCallback)
     );
 
-    rd.pipe(handleTemplateVariables)
-      .pipe(wr);
+    fs.stat(target, (err, stat) => {
+      if (err == null) {
+        // If a file at target already exists
+        promptly.confirm('Overwrite ' + target + '?', (err, confirmed) => {
+          if (confirmed) {
+            // If user confirms overwrite
+            writeFromBlueprint(blueprint, target, handleTemplateVariables, () =>
+              copyFiles(
+                blueprints,
+                __destinationDirectory__,
+                __templateDirectory__,
+                __templateName__,
+                callback,
+                index + 1
+              )
+            );
+          } else {
+            // If user denies overwrite
+            console.log('Skipping ' + target);
+
+            copyFiles(
+              blueprints,
+              __destinationDirectory__,
+              __templateDirectory__,
+              __templateName__,
+              callback,
+              index + 1
+            );
+          }
+        });
+      } else {
+        // If no file at target exists
+        writeFromBlueprint(blueprint, target, handleTemplateVariables, () =>
+          copyFiles(
+            blueprints,
+            __destinationDirectory__,
+            __templateDirectory__,
+            __templateName__,
+            callback,
+            index + 1
+          )
+        );
+      }
+    });
+  } else {
+    // If no blueprint exists for the current index
+    callback();
+  }
+};
+
+function writeFromBlueprint(blueprint, target, handleTemplateVariables, callback) {
+  const rd = fs.createReadStream(blueprint, { encoding: 'utf8' });
+  rd.on("error", err => error(err));
+
+  const wr = fs.createWriteStream(target);
+  wr.on("error", err => error(err));
+  wr.on("close", () => {
+    done(target);
+    callback()
   });
 
-  const error = err => log.error(err);
-  const done = target => success(target);
-};
+  rd.pipe(handleTemplateVariables)
+    .pipe(wr);
+}
+
+const error = err => log.error(err);
+const done = target => success(target);
